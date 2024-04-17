a8814603d6ba68873f232867afdcce8ab33572df
Maxwel <maxwellbakora@gmail.com>
2024-04-17 10:28:56 +0300

    Made fetch equest successful

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books", { timeout: 10000 }) // 10 seconds timeout
+    .then((resp) => {
+      if (!resp.ok) {
+        throw new Error("Failed to fetch books");
+      }
+      return resp.json();
+    })
+    .then((json) => renderBooks(json))
+    .catch((error) => console.error("Error fetching books:", error));
 }
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
